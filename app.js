8f17448d387b3f6c7e8b5fa2e90027ec10d377e2
jrguedes <jrguedes.ja@gmail.com>
2020-03-09 18:45:26 -0300

    Alteracao da mensagem de log no App

--- app.js
+++ app.js
@@ -2,5 +2,5 @@
 var app = require('./config/server');
 
 app.listen(3000, function () {
-    console.log('Servidor rodando com Express');
+    console.log('Servidor rodando com Express - SERVER IS ON');
 });
